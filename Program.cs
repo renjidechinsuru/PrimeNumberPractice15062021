55ea83156937c5eb0531a0045a533cd88eb6e0bd
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-15 18:37:23 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimeNumberPractice15062021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int num, count = 0;
+            Console.WriteLine("Enter any number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for(i=1; i<= num; i++)
+            {
+                if(num % i==0)
+                {
+                    count = count + 1;
+
+                }
+
+            }
+                if(count == 2)
+            {
+                Console.WriteLine("Number is Prime");
+
+            }
+                else
+            {
+                Console.WriteLine("Number is Not Prime");
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
